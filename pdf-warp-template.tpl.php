d8cbcaa40fdcb1e0569460d4a99175f6f72ddaf6
georgecardenas <jorge.cardenasc@alumnos.upm.es>
2017-06-12 22:20:18 +0200

    Template edition

--- pdf-warp-template.tpl.php
+++ pdf-warp-template.tpl.php
@@ -1,12 +1,133 @@
-<div id="pdf_warp">
+<div id="pdf_warp" class="container">
   <div ng-controller="ProductCtrl as vm">
-    <div id="pdf_container" style="height:800px">
-      <div ng-repeat="product in vm.products">
-        <h3 draggabilly>{{product.product.Title}}</h3>
-        <img draggabilly style="width:100px" ng-src="{{product.product.Image.src}}">
-        <span draggabilly>{{product.product.Price}}</span>
+    <div class="pdf-toolbar">
+      <form action="{{vm.path}}" ng-submit="vm.save()" method="post">
+        <input type="hidden" name="content" ng-value="vm.template">
+        <input class="form-submit views-ajax-processed-processed" type="submit" value="Guardar"/>
+      </form>
+    </div>
+    <div class="pdf-container">
+      <div class="pdf-template" id="pdf-template">
+        <div ng-repeat="product in vm.products">
+          <div
+               class="draggable"
+               id="pdf_product_title"
+               draggabilly
+               draggie-containment="'#pdf-template'"
+               ng-style="{
+                  left: vm.initTemplate.edit_elements.pdf_product_title.left,
+                  top: vm.initTemplate.edit_elements.pdf_product_title.top,
+                  cursor: 'pointer'
+               }"
+          >
+            {{product.product.Title}}
+          </div>
+          <div
+               class="draggable"
+               id="pdf_product_image"
+               draggabilly
+               draggie-containment="'#pdf-template'"
+               ng-style="{
+                  left: vm.initTemplate.edit_elements.pdf_product_image.left,
+                  top: vm.initTemplate.edit_elements.pdf_product_image.top,
+                  cursor: 'pointer'
+               }"
+          >
+            <img style="width:100px" ng-src="{{product.product.Image.src}}">
+          </div>
+          <div
+               class="draggable"
+               id="pdf_product_description"
+               draggabilly
+               draggie-containment="'#pdf-template'"
+               ng-style="{
+                  left: vm.initTemplate.edit_elements.pdf_product_description.left,
+                  top: vm.initTemplate.edit_elements.pdf_product_description.top,
+                  cursor: 'pointer'
+               }"
+          >
+            {{product.product.Description}}
+          </div>
+          <div
+               class="draggable"
+               id="pdf_product_price"
+               draggabilly
+               draggie-containment="'#pdf-template'"
+               ng-style="{
+                  left: vm.initTemplate.edit_elements.pdf_product_price.left,
+                  top: vm.initTemplate.edit_elements.pdf_product_price.top,
+                  cursor: 'pointer'
+               }"
+          >
+            Precio: {{product.product.Price}}
+          </div>
+          <div
+               class="draggable"
+               id="pdf_product_sku"
+               draggabilly
+               draggie-containment="'#pdf-template'"
+               ng-style="{
+                  left: vm.initTemplate.edit_elements.pdf_product_sku.left,
+                  top: vm.initTemplate.edit_elements.pdf_product_sku.top,
+                  cursor: 'pointer'
+               }"
+
+               >SKU: {{product.product.SKU}}
+          </div>
+          <div
+               class="draggable"
+               id="pdf_product_length"
+               draggabilly
+               draggie-containment="'#pdf-template'"
+               ng-style="{
+                  left: vm.initTemplate.edit_elements.pdf_product_length.left,
+                  top: vm.initTemplate.edit_elements.pdf_product_length.top,
+                  cursor: 'pointer'
+               }"
+          >
+            Largo: {{product.product.Length}}
+          </div>
+          <div
+               class="draggable"
+               id="pdf_product_width"
+               draggabilly
+               draggie-containment="'#pdf-template'"
+               ng-style="{
+                  left: vm.initTemplate.edit_elements.pdf_product_width.left,
+                  top: vm.initTemplate.edit_elements.pdf_product_width.top,
+                  cursor: 'pointer'
+               }"
+          >
+            Ancho: {{product.product.Width}}
+          </div>
+          <div
+               class="draggable"
+               id="pdf_product_height"
+               draggabilly
+               draggie-containment="'#pdf-template'"
+               ng-style="{
+                  left: vm.initTemplate.edit_elements.pdf_product_height.left,
+                  top: vm.initTemplate.edit_elements.pdf_product_height.top,
+                  cursor: 'pointer'
+               }"
+          >
+            Alto: {{product.product.Height}}
+          </div>
+          <div
+               class="draggable"
+               id="pdf_product_weight"
+               draggabilly
+               draggie-containment="'#pdf-template'"
+               ng-style="{
+                  left: vm.initTemplate.edit_elements.pdf_product_weight.left,
+                  top: vm.initTemplate.edit_elements.pdf_product_weight.top,
+                  cursor: 'pointer'
+               }"
+          >
+            Peso: {{product.product.Weight}}
+          </div>
+        </div>
       </div>
     </div>
-    <button ng-click="vm.toPDF()" id="toPDFButton">Generate PDF</button>
   </div>
 </div>
--- pdf_warp.js
+++ pdf_warp.js
@@ -1,5 +1,6 @@
 var app = angular
   .module('pdf_warp',['ngRoute','draggabilly'])
+
   .config(function ($locationProvider) 
   {
       $locationProvider.html5Mode({
@@ -7,14 +8,42 @@ var app = angular
         requireBase: false
       });
    })
-  .controller('ProductCtrl', function($http, $location) {
+
+  .controller('ProductCtrl', function($http, $location, $rootScope) {
     var vm = this;
     vm.products = [];
+    vm.path = $location.path() + '/save';
+    vm.template = "";
+    vm.initTemplate = "";
     
     $http.get('http://localhost/warpdf/store/product/2/json')
     .then(function(result) {
       vm.products = result.data.products;
+      var templateInfo = Drupal.settings.pdf_warp.template_content;
+      vm.initTemplate = JSON.parse(templateInfo[0].content);
     });
+    
+    vm.save = function() {
+      var elements = document.querySelectorAll('*[id^="pdf_product_"]');
+      var containerBounds = document.getElementById("pdf-template").getBoundingClientRect();
+      var template = {
+        pdf_elements: {},
+        edit_elements: {}
+      }
+      angular.forEach(elements, function(element, index){
+        var elementBounds = element.getBoundingClientRect();
+        template.pdf_elements[element.id] = {
+          x: elementBounds.left - containerBounds.left,
+          y: elementBounds.top - containerBounds.top
+        };
+        template.edit_elements[element.id] = {
+          left: element.style.left || '0px',
+          top: element.style.top || '0px'
+        };
+      });
+      
+      vm.template = JSON.stringify(template);
+    }
     
     vm.toPDF = function() {
       var element = document.getElementById('pdf_container');
